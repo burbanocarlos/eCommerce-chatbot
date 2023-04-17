8164e8e429eb433cee5b408e29a4933779033eca
Carlos Burbano <burbanocarlos1000@gmail.com>
2023-04-16 19:11:24 -0500

    added second version of chatbot

--- ChatBot/main.py
+++ ChatBot/main.py
@@ -81,7 +81,7 @@ model = tflearn.DNN(net)
 # try:
 #     model.load("model.tflearn")
 # except:
-model.fit(training, output, n_epoch=2000, batch_size=16,show_metric=True )
+model.fit(training, output, n_epoch=1000, batch_size=16,show_metric=True )
 model.save("model.tflearn")
 
 def bag_of_words(s, words):
